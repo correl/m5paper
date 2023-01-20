4d828e874e177d6be37596c16fa672a0f293f484
Correl Roush <correl@gmail.com>
2023-01-19 22:41:50 -0500

    Add an OTA update webserver

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,38 @@
 #include <M5EPD.h>
+#include <WiFi.h>
+#include <ESPAsyncWebServer.h>
+#include <AsyncElegantOTA.h>
 
-void setup()
-{
+const char* ssid = "REPLACE_WITH_YOUR_SSID";
+const char* password = "REPLACE_WITH_YOUR_PASSWORD";
+
+AsyncWebServer server(80);
+
+void setup(void) {
+    Serial.begin(115200);
+    WiFi.mode(WIFI_STA);
+    WiFi.begin(ssid, password);
+    Serial.println("");
+
+    // Wait for connection
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        Serial.print(".");
+    }
+    Serial.println("");
+    Serial.print("Connected to ");
+    Serial.println(ssid);
+    Serial.print("IP address: ");
+    Serial.println(WiFi.localIP());
+
+    server.on("/", HTTP_GET, [](AsyncWebServerRequest *request) {
+        request->send(200, "text/plain", "Hi! I am ESP32.");
+    });
+
+    AsyncElegantOTA.begin(&server);    // Start ElegantOTA
+    server.begin();
+    Serial.println("HTTP server started");
+
     disableCore0WDT();
     M5.begin();
     M5.TP.SetRotation(90);
