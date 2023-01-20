324befebb998063fc0bfd0841b1e7feadfda470b
Correl <correl@gmail.com>
2023-01-20 11:26:41 -0500

    Add an OTA update server

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,6 @@
 #include <M5EPD.h>
 #include <WiFi.h>
-#include <ESPAsyncWebServer.h>
+#include <ESPAsyncWebSrv.h>
 #include <AsyncElegantOTA.h>
 
 const char* ssid = "REPLACE_WITH_YOUR_SSID";
@@ -9,8 +9,17 @@ const char* password = "REPLACE_WITH_YOUR_PASSWORD";
 AsyncWebServer server(80);
 M5EPD_Canvas canvas(&M5.EPD);
 
+void setup_wifi() {
+}
+
 void setup(void) {
-    Serial.begin(115200);
+    M5.begin();
+    M5.EPD.SetRotation(90);
+    M5.EPD.Clear(true);
+    M5.RTC.begin();
+    Serial.println("createCanvas");
+    canvas.createCanvas(540, 960);
+
     WiFi.mode(WIFI_STA);
     WiFi.begin(ssid, password);
     Serial.println("");
@@ -34,13 +43,6 @@ void setup(void) {
     server.begin();
     Serial.println("HTTP server started");
 
-    disableCore0WDT();
-    M5.begin();
-    M5.TP.SetRotation(90);
-    M5.EPD.SetRotation(90);
-    M5.EPD.Clear(true);
-    M5.RTC.begin();
-    canvas.createCanvas(400, 300);
     canvas.setTextSize(3);
     canvas.drawString(ssid, 0, 0);
     canvas.drawString(WiFi.localIP().toString(), 0, 20);
@@ -49,4 +51,5 @@ void setup(void) {
 
 void loop()
 {
+    yield();
 }
